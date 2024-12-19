73cde6f5a4be20dace1a77ee593fab6bc2fab596
Gökhan Özdemir <gn.ozdemir@gmail.com>
2024-12-19 17:34:46 +0300

    pill şekli için variant ekler

--- src/components/ui/button.jsx
+++ src/components/ui/button.jsx
@@ -26,21 +26,31 @@ const buttonVariants = cva(
         lg: "h-10 rounded-md px-8",
         icon: "h-9 w-9",
       },
+      shape: {
+        rounded: "rounded-sm",
+        pill: "rounded-full",
+        square: "rounded-none",
+      },
     },
     defaultVariants: {
       variant: "default",
       size: "default",
+      shape: "rounded",
     },
   }
 );
 
-const Button = React.forwardRef(({ className, variant, size, asChild = false, ...props }, ref) => {
-  const Comp = asChild ? Slot : "button"
+const Button = React.forwardRef(
+  ({ className, variant, size, shape, asChild = false, ...props }, ref) => {
+    const Comp = asChild ? Slot : "button";
     return (
-    (<Comp
-      className={cn(buttonVariants({ variant, size, className }))}
+      <Comp
+        className={cn(buttonVariants({ variant, size, shape, className }))}
         ref={ref}
-      {...props} />)
+        {...props}
+      />
+    );
+  }
 );
 Button.displayName = "Button";
 
